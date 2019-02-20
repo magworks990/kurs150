9075e231b672376d85a0e6364e7edb99019f692d
Pawel Ziemak <pawel.ziemak@idebank.pl>
2019-02-20 15:27:58 +0100

    drugiBranch

--- src/Wyswietlenia.java
+++ src/Wyswietlenia.java
@@ -21,6 +21,9 @@ public class Wyswietlenia {
 
 //            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#u_0_l > div:nth-child(2) > a"))).click();
             Thread.sleep(2500);
+            Thread.sleep(2500);
+            Thread.sleep(2500);
+            Thread.sleep(2500);
             driver.close();
         }
     }
